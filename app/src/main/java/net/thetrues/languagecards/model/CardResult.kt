c6cc2d88e4f111edd137459656ee83410c3555ce
John True <johnmtrue@gmail.com>
2026-03-03 20:52:52 -0800

    Cleanup old app directory

--- app/src/main/java/net/thetrues/languagecards/model/CardResult.kt
+++
@@ -1,9 +0,0 @@
-package net.thetrues.languagecards.model
-
-/**
- * Result of one card in a practice session: hit (correct) or miss.
- */
-data class CardResult(
-    val cardId: String,
-    val wasHit: Boolean,
-)
